fec2df40dcae8d7211b9b4592573f789a1ae0616
chanhalee <chan771788@gmail.com>
2024-04-12 17:42:54 +0900

    feat(RequestHandler) HttpRequest 생성에 따른 RequestHandler 역할 축소
    HttpRequest 생성에 따른 RequestHandler 역할 축소

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,20 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import webserver.http.HttpRequest;
+import webserver.http.HttpResponse;
+import webserver.mvc.controller.Controller;
+import webserver.mvc.RequestMapping;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,14 +28,15 @@ public class RequestHandler implements Runnable {
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            HttpRequest request = new HttpRequest(br);
+            HttpResponse response = new HttpResponse();
+            Controller controller = RequestMapping.getRequestMapping(request);
+
+            controller.service(request, response);
+            response.send(out);
+        } catch (Exception e) {
             logger.error(e.getMessage());
         }
     }
